aaf2770c1b9692d61a52ff129e6f16152d14caed
Josué Nzanza <nzanzajosue5@gmail.com>
2024-05-17 03:31:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let mybutton=document.getElementById("myBtn");
+window.onscroll=
+function ()
+{ scrollFunction()};
+function scrollFunction(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {mybutton.style.display="block";} else { mybutton.style.display="none";
+        
+ }
+}
+function  topFunction (){
+document.body.scroll=0;  document.documentElement.scrollTop=0;
+}
